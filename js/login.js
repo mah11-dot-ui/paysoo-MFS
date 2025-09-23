712d28f869c981637c7ac940b981abc61ee3cb30
mah11-dot-ui <mhassanwbdpr+++++@gmail.com>
2025-09-23 11:00:16 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,8 @@
+// console.log('login button clicked')
+
+document.getElementById('login-button').addEventListener('click', function(event){
+    event.preventDefault();  // <--- Vejaila To Begainears --->
+    console.log('login button clicked')
+    const PhoneNumber = document.getElementById('phone-number');
+    console.log(PhoneNumber)
+})
