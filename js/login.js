5ec961a903c6a15f57f7dcade7e39621503e9668
mah11-dot-ui <mhassanwbdpr+++++@gmail.com>
2025-09-24 08:08:14 +0600

    added home file andchanged index.html and login.js

--- js/login.js
+++ js/login.js
@@ -1,8 +1,20 @@
 // console.log('login button clicked')
 
 document.getElementById('login-button').addEventListener('click', function(event){
+    window.location.href = '/home.html'
     event.preventDefault();  // <--- Vejaila To Begainears --->
     console.log('login button clicked')
     const PhoneNumber = document.getElementById('phone-number');
-    console.log(PhoneNumber)
+    const pinNumber = document.getElementById('pin-number');    
+   
+    const floatNumber = document.getElementById('bank-float')
+     console.log(PhoneNumber, pinNumber, floatNumber)
+
+    if(PhoneNumber=== '5' && pinNumber=== '1234' && floatNumber=== '1'){
+        console.log('you are loged in')
+    }
+    else{
+        alert('Wrong phone number and pin')
+    }
+
 })
