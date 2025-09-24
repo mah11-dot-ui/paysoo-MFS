f4738b313bb101178e214891d6494ecdd9f9d4ff
mah11-dot-ui <mhassanwbdpr+++++@gmail.com>
2025-09-24 08:41:55 +0600

    changed home file

--- js/home.js
+++ js/home.js
@@ -1,1 +1,9 @@
-console.log('home added')
+
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+    const AddMoneyInput = document.getElementById('input-add-money');
+    console.log(AddMoneyInput)
+
+    const PinNumberInput = document.getElementById('input-pin-number')
+    console.log(PinNumberInput)
+})
